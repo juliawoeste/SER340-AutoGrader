88388146c55be5882ada686a71b386d97f51fb85
Humna Hanif <hhanif@quinnipiac.edu>
2023-05-02 11:07:41 -0400

    hidden password field

--- Milestone3/learning-management-app/src/components/LoginFormProfessor.jsx
+++ Milestone3/learning-management-app/src/components/LoginFormProfessor.jsx
@@ -8,13 +8,13 @@ import "./styles/login-style.css";
 import { Link } from "react-router-dom";
 
 class LoginFormProfessor extends Component {
-  //username = React.createRef();
   state = {
     account: {
       email: "",
       password: "",
     },
     errors: {},
+    disableSubmit: true,
   };
   schema = {
     email: Joi.string().required().label("Email"),
@@ -25,7 +25,6 @@ class LoginFormProfessor extends Component {
     const { error } = Joi.validate(this.state.account, this.schema, {
       abortEarly: false,
     });
-    if (!error) return null;
     const errors = {};
     for (let item of error.details) errors[item.path[0]] = item.message;
     return errors;
@@ -66,88 +65,67 @@ class LoginFormProfessor extends Component {
   render() {
     return (
       <section className="glasscard">
-        <div>
-          <div
-            className="container"
-            style={{
-              width: "30rem",
-              alignContent: "center",
-              textAlign: "center",
-            }}
-          >
-            <form onSubmit={this.handleSubmit}>
-              <div className="form-group">
-                <h4>Professor Login</h4>
-                <h6 style={{ marginTop: "2rem" }}>
-                  Sign in to your account to continue.
-                </h6>
-                <div className="row">
-                  <label
-                    style={{
-                      fontWeight: "bold",
-                      marginTop: "2rem",
-                      textAlign: "center",
-                    }}
-                    htmlFor="email"
-                  >
-                    Email Address
-                  </label>
-                  <input
-                    value={this.state.account.email}
-                    onChange={this.handleChange}
-                    name="email"
-                    id="email"
-                    type="text"
-                    aria-aria-describedby="emailHelp"
-                    className="form-control"
-                    placeholder="Enter Email"
-                  />
+        <div
+          className="container"
+          style={{
+            width: "30rem",
+            alignContent: "center",
+            textAlign: "center",
+          }}
+        >
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <h4>Professor Login</h4>
+              <h6 style={{ marginTop: "2rem" }}>
+                Sign in to your account to continue.
+              </h6>
+              <label htmlFor="email">Email Address</label>
+              <input
+                value={this.state.account.email}
+                onChange={this.handleChange}
+                name="email"
+                id="email"
+                type="text"
+                className="form-control"
+                placeholder="Enter Email"
+              />
+              {this.state.errors.email && (
+                <div className="alert alert-danger">
+                  {" "}
+                  {this.state.errors.email}
                 </div>
-                {this.state.errors.email && (
-                  <div className="alert alert-danger">
-                    {" "}
-                    {this.state.errors.email}
-                  </div>
-                )}
-              </div>
-              <div className="form-group">
-                <label
-                  style={{
-                    fontWeight: "bold",
-                    marginTop: "2rem",
-                    textAlign: "center",
-                  }}
-                  htmlFor="password"
+              )}
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                id="password"
+                value={this.state.account.password}
+                onChange={this.handleChange}
+                name="password"
+                type="password"
+                className="form-control"
+                placeholder="Enter Password"
+              />
+              {this.state.errors.password && (
+                <div className="alert alert-danger">
+                  {" "}
+                  {this.state.errors.password}
+                </div>
+              )}
+            </div>
+            <div className="form-group">
+              <Link to={"/professorCourses"}>
+                <button
+                  className="btn btn-primary"
+                  style={{ marginTop: "2rem" }}
+                  disabled={this.state.disableSubmit}
                 >
-                  Password
-                </label>
-                <input
-                  id="password"
-                  value={this.state.account.password}
-                  onChange={this.handleChange}
-                  name="password"
-                  type="text"
-                  aria-aria-describedby="emailHelp"
-                  className="form-control"
-                  placeholder="Enter Password"
-                />
-                {this.state.errors.password && (
-                  <div className="alert alert-danger">
-                    {" "}
-                    {this.state.errors.password}
-                  </div>
-                )}
-                <Link to={"/professorCourses"}>
-                  <button
-                    className="btn btn-primary"
-                    style={{ marginTop: "2rem" }}
-                  >
-                    Login
-                  </button>
-                </Link>
-              </div>
-            </form>
-          </div>
+                  Login
+                </button>
+              </Link>
+            </div>
+          </form>
         </div>
       </section>
     );
