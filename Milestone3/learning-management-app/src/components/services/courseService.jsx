c05290adbcc5409e853421cc16feb62d60c8ff1e
McLaren <ramclaren@quinnipiac.edu>
2023-05-01 20:49:13 -0400

    addedd course card implementation

---
+++ Milestone3/learning-management-app/src/components/services/courseService.jsx
@@ -0,0 +1,25 @@
+const courses = [
+  {
+    id: 1,
+    name: "SER341",
+    summary: "It's a course",
+  },
+  {
+    id: 2,
+    name: "SER120",
+    summary: "It's a course",
+  },
+  {
+    id: 3,
+    name: "SER490",
+    summary: "It's a course",
+  },
+  {
+    id: 4,
+    name: "SER200",
+    summary: "It's a course",
+  },
+];
+export function getCourses() {
+  return courses;
+}
