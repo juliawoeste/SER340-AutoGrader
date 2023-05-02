58afbb760d27aaa72d29fd58c7f855d24953eb29
Humna Hanif <hhanif@quinnipiac.edu>
2023-05-01 21:54:56 -0400

    add student styling

--- Milestone3/learning-management-app/src/components/AddStudent.jsx
+++ Milestone3/learning-management-app/src/components/AddStudent.jsx
@@ -14,52 +14,54 @@ class AddStudent extends Component {
     return (
       <div>
         <ProfessorNavbar />
-        <form>
-          <div className="form-group" style={{ paddingBottom: "2rem" }}>
-            <h4>Add Student</h4>
-            <h6 style={{ marginTop: "2rem" }}>
-              Please input the following information to add a student.
-            </h6>
-            <label for="name" style={{ paddingTop: "2rem" }}>
-              Student Name
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="Enter Student Name"
-              style={{ width: "15rem" }}
-            />
-            <label for="email" style={{ paddingTop: "2rem" }}>
-              Student Email
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="Enter Student Email"
-              style={{ width: "15rem" }}
-            />
-            <label for="password" style={{ paddingTop: "2rem" }}>
-              Student Password
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="Enter Student Password"
-              style={{ width: "15rem" }}
-            />
-          </div>
-          <button type="submit" className="btn btn-primary">
-            <NavLink className="nav-link" to="/professorCourses">
-              Submit
-            </NavLink>
-          </button>
-        </form>
+        <section className="glasscard">
+          <form>
+            <div className="form-group" style={{ paddingBottom: "2rem" }}>
+              <h4>Add Student</h4>
+              <h6 style={{ marginTop: "2rem" }}>
+                Please input the following information to add a student.
+              </h6>
+              <label for="name" style={{ paddingTop: "2rem" }}>
+                Student Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter Student Name"
+                style={{ width: "15rem", marginLeft: "8rem" }}
+              />
+              <label for="email" style={{ paddingTop: "2rem" }}>
+                Student Email
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter Student Email"
+                style={{ width: "15rem", marginLeft: "8rem" }}
+              />
+              <label for="password" style={{ paddingTop: "2rem" }}>
+                Student Password
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter Student Password"
+                style={{ width: "15rem", marginLeft: "8rem" }}
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              <NavLink className="nav-link" to="/professorCourses">
+                Submit
+              </NavLink>
+            </button>
+          </form>
+        </section>
       </div>
     );
   }
